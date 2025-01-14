1b6b3c1b8972100067dd5104da03c1ce67ba6466
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-01-14 11:39:50 +0100

    [MAIN][NEW]Antidote test

--- __test__/__mocks__/antidoteIngredients.ts
+++ __test__/__mocks__/antidoteIngredients.ts
@@ -2,7 +2,7 @@ export const restoreIngredient1 = {
     _id: "6702b44f76863c206a48cce6",
     name: "Courageous Bloom",
     description:
-    "A bloom that invigorates the constitution and instills bravery.",
+        "A bloom that invigorates the constitution and instills bravery.",
     value: 105,
     effects: ["restore_constitution"],
     image: "/images/ingredients/restore/restore_17.webp",
@@ -12,15 +12,55 @@ export const restoreIngredient1 = {
 };
 
 export const restoreIngredient2 = {
-    "_id": "6702b44f76863c206a48cce7",
-    "name": "Silver Petal",
-    "description": "A delicate petal that enhances charisma, making interactions smoother.",
-    "value": 30,
-    "effects": [
-    "lesser_restore_charisma"
-    ],
-    "image": "/images/ingredients/restore/restore_18.webp",
-    "type": "ingredient",
+    _id: "6702b44f76863c206a48cce7",
+    name: "Silver Petal",
+    description:
+        "A delicate petal that enhances charisma, making interactions smoother.",
+    value: 30,
+    effects: ["lesser_restore_charisma"],
+    image: "/images/ingredients/restore/restore_18.webp",
+    type: "ingredient",
     key: "b",
     qty: 1,
 };
+export const curse = {
+    modifiers: {
+        hit_points: 0,
+        intelligence: 0,
+        dexterity: 0,
+        insanity: 0,
+        charisma: -8,
+        constitution: -12,
+        strength: 0,
+    },
+    _id: "6693fd5846527d0df5f0efe8",
+    name: "Frostbane Fever",
+    description:
+        "A chilling illness that lowers the body temperature drastically, causing frost to form on the skin and organs to freeze.",
+    type: "illness",
+    antidote_effects: ["restore_constitution", "lesser_restore_charisma"],
+    poison_effects: ["damage_constitution", "lesser_damage_charisma"],
+};
+
+export const ingredientNoRestore = {
+    _id: "6702b46b76863c206a48ccfd",
+    name: "Crippling Bloom",
+    description:
+        "A flower that weakens constitution and breaks the will of its user.",
+    value: 60,
+    effects: ["damage_constitution"],
+    image: "/images/ingredients/damage/damage_11.webp",
+    type: "ingredient",
+};
+
+export const ingredientNoRestore2 = {
+    "_id": "6702b46b76863c206a48cd05",
+    "name": "Gloom Petal",
+    "description": "A petal that shrouds charisma in shadows, making one unlikable.",
+    "value": 160,
+    "effects": [
+    "greater_damage_charisma"
+    ],
+    "image": "/images/ingredients/damage/damage_19.webp",
+    "type": "ingredient"
+}
---
+++ __test__/antidote.test.js
@@ -0,0 +1,82 @@
+import {
+    restoreIngredient1,
+    restoreIngredient2,
+    ingredientNoRestore, 
+    ingredientNoRestore2
+} from "./__mocks__/antidoteIngredients";
+import Cauldron from "../potions/cauldron";
+import Ingredients from "../potions/ingredients";
+import Curses from "../potions/curses";
+import { describe, expect, test } from "@jest/globals";
+
+describe("Cuando todos los ingredientes llevan el efecto Restore", () => {
+    describe("Si los ingredientes contienen los efectos necesarios para combatir una enfermedad concreta se creeara el antidoto asociado a la enfermedad", () => {
+        it('El nombre debera ser el correspondiente. Antidote of + "nombre de la enfermedad"', () => {
+            // Arrange
+            const fakeIngredients = require("./__mocks__/fake-ingredients.json");
+            const fakeCurses = require("./__mocks__/fake-curses.json");
+
+            const ingredients = Ingredients.load(fakeIngredients).ingredients;
+            const curses = Curses.load(fakeCurses).curses;
+
+            const ingredientsArray = [restoreIngredient1, restoreIngredient2];
+
+            const cauldron = new Cauldron(ingredients, curses);
+
+            // Act
+            const potion = cauldron.createPotion(ingredientsArray);
+            console.log(potion);
+
+            // Assert
+            expect(potion.name).toBe("Antidote of Frostbane Fever");
+        });
+
+        it("Los atributos tendran el valor que aparece en la enfermedad pero cambiado de signo o, en su defecto, el rango de valores que se muestra en la tabla de creacion de pociones (si se ha empleado)", () => {
+            // Arrange
+            const fakeIngredients = require("./__mocks__/fake-ingredients.json");
+            const fakeCurses = require("./__mocks__/fake-curses.json");
+
+            const ingredients = Ingredients.load(fakeIngredients).ingredients;
+            const curses = Curses.load(fakeCurses).curses;
+
+            const ingredientsArray = [restoreIngredient1, restoreIngredient2];
+
+            const cauldron = new Cauldron(ingredients, curses);
+
+            // Act
+            const potion = cauldron.createPotion(ingredientsArray);
+
+            // Assert
+            console.log(potion.modifiers);
+            expect(potion.modifiers.constitution).toBeGreaterThan(9);
+            expect(potion.modifiers.constitution).toBeLessThan(14);
+
+            expect(potion.modifiers.charisma).toBeGreaterThan(5);
+            expect(potion.modifiers.constitution).toBeLessThan(10);
+
+        });
+    });
+});
+
+describe('Si alguno de los ingredientes no tiene el nombre Restore', () => {
+
+    it('No podremos crear un antidoto. El nombre de la pocion creada no llevara la palabra "Antidote"', () => {
+        // Arrange
+        const fakeIngredients = require("./__mocks__/fake-ingredients.json");
+        const fakeCurses = require("./__mocks__/fake-curses.json");
+
+        const ingredients = Ingredients.load(fakeIngredients).ingredients;
+        const curses = Curses.load(fakeCurses).curses;
+
+        const ingredientsArray = [ingredientNoRestore, ingredientNoRestore2];
+
+        const cauldron = new Cauldron(ingredients, curses);
+
+        // Act
+        const potion = cauldron.createPotion(ingredientsArray);
+
+        // Assert
+        expect(potion.name).not.toContain('Antidote');
+    });
+});
+
